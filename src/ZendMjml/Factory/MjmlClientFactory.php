2e0b3b28cb3fe860650ff597b6aa895bec12a44d
n.yotsov <karboratorr@gmail.com>
2017-07-12 15:24:41 +0300

    - Factories supports ZF3  - Composer ZF3

--- src/ZendMjml/Factory/MjmlClientFactory.php
+++ src/ZendMjml/Factory/MjmlClientFactory.php
@@ -4,11 +4,37 @@ namespace ZendMjml\Factory;
 
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Subscriber\Cache\CacheSubscriber;
-use Zend\ServiceManager\FactoryInterface;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class MjmlClientFactory implements FactoryInterface
 {
+    /**
+     * @param ContainerInterface $container
+     * @param string             $requestedName
+     * @param array|null         $options
+     *
+     * @return GuzzleClient
+     */
+    public function __invoke(ContainerInterface $container, $requestedName,
+        array $options = null
+    ) {
+        $config = $container->get('Config')['mjml'];
+
+        $client = new GuzzleClient([
+            'base_url' => rtrim($config['mjmlServiceUrl'], '/'),
+            'defaults' => [
+                'timeout' => $config['timeout'],
+                'connect_timeout' => $config['connectTimeout'],
+            ],
+        ]);
+
+        CacheSubscriber::attach($client);
+
+        return $client;
+    }
+
     /**
      * Create service
      *
--- src/ZendMjml/Factory/MjmlFactory.php
+++ src/ZendMjml/Factory/MjmlFactory.php
@@ -2,12 +2,42 @@
 
 namespace ZendMjml\Factory;
 
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
 use ZendMjml\Service\Mjml;
-use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class MjmlFactory implements FactoryInterface
 {
+    /**
+     * @param ContainerInterface $container
+     * @param string             $requestedName
+     * @param array|null         $options
+     *
+     * @return Mjml
+     * @throws \Exception
+     */
+    public function __invoke(ContainerInterface $container, $requestedName,
+        array $options = null
+    ) {
+        $config        = $container->get('Config')['mjml'];
+        $httpClient    = $container->get('Client\Mjml');
+        $renderer      = $container->get('Zend\View\Renderer\RendererInterface');
+        $adapterConfig = $config['transportAdapter'];
+        $transport     = null;
+
+        if (is_string($adapterConfig)) {
+            $transport = $container->get($adapterConfig);
+        } elseif (is_array($adapterConfig)) {
+            $transport = \Zend\Mail\Transport\Factory::create($adapterConfig);
+        }
+        if (null === $transport) {
+            throw new \Exception('Transport Adapter cannot be found.');
+        }
+
+        return new Mjml($httpClient, $renderer, $transport);
+    }
+
     /**
      * Create service
      *
